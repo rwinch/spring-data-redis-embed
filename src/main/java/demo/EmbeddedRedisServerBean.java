f0d181397642a966c6383588ce4e51481a15fca1
Rob Winch <rwinch@gopivotal.com>
2015-07-24 11:06:09 -0500

    Initial

---
+++ src/main/java/demo/EmbeddedRedisServerBean.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2011-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package demo;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.util.Assert;
+import org.springframework.util.SocketUtils;
+
+import redis.embedded.RedisServer;
+
+/**
+ *
+ * @author Rob Winch
+ * @since 1.6
+ */
+public final class EmbeddedRedisServerBean implements InitializingBean, DisposableBean {
+	private final RedisServer redisServer;
+
+	public EmbeddedRedisServerBean(RedisServer redisServer) {
+		Assert.notNull(redisServer, "RedisServer may not be null");
+		this.redisServer = redisServer;
+	}
+
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("Starting on " + getPort());
+		redisServer.start();
+	}
+
+	public void destroy() throws Exception {
+		if(redisServer != null) {
+			System.out.println("Shutting down on " + getPort());
+			redisServer.stop();
+		}
+	}
+
+	public RedisServer getRedisServer() {
+		return redisServer;
+	}
+
+	public int getPort() {
+		return redisServer.ports().get(0);
+	}
+
+	public static EmbeddedRedisServerBean createWithAvailbeTcpPort() throws IOException {
+		RedisServer redisServer = new RedisServer(SocketUtils.findAvailableTcpPort());
+		return new EmbeddedRedisServerBean(redisServer);
+	}
+}
---
+++ src/test/java/demo/EmbeddedRedisServerBeanIntegrationTests.java
@@ -0,0 +1,78 @@
+package demo;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import redis.embedded.RedisServer;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+public class EmbeddedRedisServerBeanIntegrationTests {
+	@Autowired
+	RedisTemplate<String, String> redis;
+
+	@Autowired
+	RedisConnectionFactory connectionFactory;
+
+	@Test
+	public void version() {
+		Properties info = redis.execute(new RedisCallback<Properties>() {
+			public Properties doInRedis(RedisConnection connection) throws DataAccessException {
+				return connection.info("Server");
+			}
+		});
+
+		String redisVersion = (String) info.get("redis_version");
+		System.out.println("======================");
+		System.out.println(redisVersion);
+		String[] parts = redisVersion.split("\\.");
+		int majorVersion = Integer.parseInt(parts[0]);
+		int minorVersion = Integer.parseInt(parts[1]);
+
+		boolean is3xOrLarger = majorVersion >= 3;
+		boolean is2Dot8OrLarger = is3xOrLarger || (majorVersion >= 2 && minorVersion >= 8);
+
+		assertTrue("Expected 2.8.x or larger. Got "+ redisVersion, is2Dot8OrLarger);
+	}
+
+	@Configuration
+	static class Config {
+
+		@Bean
+		public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+			RedisTemplate<String, String> redis = new RedisTemplate<String, String>();
+			redis.setConnectionFactory(connectionFactory);
+			return redis;
+		}
+
+		@Bean
+		public JedisConnectionFactory connectionFactory(@Value("#{@embeddedRedis.port}") int port) {
+			JedisConnectionFactory connection = new JedisConnectionFactory();
+			connection.setPort(port);
+			return connection;
+		}
+
+		@Bean
+		public EmbeddedRedisServerBean embeddedRedis() throws IOException {
+			return new EmbeddedRedisServerBean(new RedisServer());
+		}
+	}
+
+}
